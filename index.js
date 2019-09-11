f197e92efc04499282875cdf3365f779e36c82cf
Diogo Agra Alexandria <diogoaa@ipiranga.ipiranga>
2019-09-11 18:42:13 -0300

    Conexão com Mongo e lógica de percorrer árvore de decisão

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var pug = require('pug');
+let express = require('express');
+let bodyParser = require('body-parser');
+let mongoose = require('mongoose')
 
-var app = express();
+let app = express();
 const PORT = 3000;
+
+mongoose.connect('mongodb://localhost:27017/int_comp', {useNewUrlParser: true, useUnifiedTopology: true})
+let db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Connected to MongoDB') 
+});
 
 app.set('view engine', 'pug')
 app.use(bodyParser.json());
@@ -14,25 +22,51 @@ var requestTime = function (req, res, next) {
     next();
 };
 
-app.use(requestTime);
-app.use('/', (req, res, next) => {    
-    let responseText = 'Requested at: ' + req.requestTime + '';
-    console.log(responseText)
-    next();
-})
-app.get('/', (req, res, next) => {    
-    res.render('index', {
-        title:'Teste',
-        botao_1: 'botao 1',
-        botao_2: 'botao 2'
-    })       
+let Node = require('./models/node')
+
+app.get('/', (req, res, next) => {
+    let node_name = req.query.name || 'Raiz'    
+    Node.find({$or: [{name: node_name}, {father: node_name}]}, (err, nodes) => {
+        if(!err) {
+            var button_condition = true
+            var node_message
+            var right_button
+            var left_button
+            nodes.map((node) => {                
+                if(node.name === node_name && !!node.question){                    
+                    node_message = node.question
+                } else if(node.name === node_name) {                    
+                    button_condition = false
+                    node_message = "Você pensou em um" + node.termination + " " + node.name 
+                } else if(node.last_answer == true) {                    
+                    left_button = node.name
+                } else {                    
+                    right_button = node.name
+                }
+            })       
+            console.log({node_message, left_button, right_button})
+            res.render('index', {
+                message: node_message,
+                button_yes: left_button,
+                button_no: right_button,
+                final_answer: button_condition
+            })           
+        } else {
+            console.log(err)
+        }
+    })           
 });
 
-app.post('/answer', (req, res) => {    
-    res.json({
-        "Mensagem": "Ok"
+app.post('/answer', (req, res) => {   
+    
+    Node.find({$or: [{node: req.body.name}, {father: req.body.name}]}, (err, node) => {
+        if(!err) {            
+            res.redirect(`/?name=${req.body.name}`)           
+        } else {
+            console.log(err)
+        }
     })
-    console.log(req.body)
+    
 })
 
 app.listen(PORT, () =>{
